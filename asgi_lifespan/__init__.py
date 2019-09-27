d564bb82319a295fc1c8ba7a2898a653dd317241
florimondmanca <florimond.manca@gmail.com>
2019-09-27 22:33:41 +0200

    Run lint

--- asgi_lifespan/__init__.py
+++ asgi_lifespan/__init__.py
@@ -1,3 +1,3 @@
-from .__version__ import __title__, __description__, __version__
+from .__version__ import __description__, __title__, __version__
 
 __all__ = ["__description__", "__title__", "__version__"]
