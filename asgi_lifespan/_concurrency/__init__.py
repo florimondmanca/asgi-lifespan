31f44ecd3a3918c0a26d093bfbc93c686476d119
florimondmanca <florimond.manca@gmail.com>
2020-02-02 11:56:23 +0100

    Refine auto-detection

--- asgi_lifespan/_concurrency/__init__.py
+++ asgi_lifespan/_concurrency/__init__.py
@@ -1,22 +1,57 @@
+import sys
+
 from .base import ConcurrencyBackend
 
 
+def _is_already_imported(module: str) -> bool:
+    return module in sys.modules
+
+
+def sniff_async_library() -> str:
+    # Adapted from:
+    # https://github.com/python-trio/sniffio/blob/master/sniffio/_impl.py
+
+    if _is_already_imported("asyncio"):
+        import asyncio
+
+        try:
+            current_task = asyncio.current_task
+        except AttributeError:
+            current_task = asyncio.Task.current_task
+
+        try:
+            task = current_task()
+        except RuntimeError:
+            pass
+        else:
+            if task is not None:
+                return "asyncio"
+
+    if _is_already_imported("trio"):
+        import trio
+
+        try:
+            task = trio.hazmat.current_task()
+        except RuntimeError:
+            pass
+        else:
+            assert task is not None
+            return "trio"
+
+    raise NotImplementedError(
+        "Unknown async library, or not in async context."
+    )  # pragma: no cover
+
+
 def detect_concurrency_backend() -> ConcurrencyBackend:
-    from .asyncio import AsyncioBackend
-
-    try:
-        import sniffio
-    except ImportError:  # pragma: no cover
-        # sniffio would have been installed with trio,
-        # so we must be running on asyncio.
-        return AsyncioBackend()
-
-    library = sniffio.current_async_library()
+    library = sniff_async_library()
     assert library in ("asyncio", "trio")
 
-    if library == "asyncio":
+    if library == "trio":
+        from .trio import TrioBackend
+
+        return TrioBackend()
+    else:
+        from .asyncio import AsyncioBackend
+
         return AsyncioBackend()
-
-    from .trio import TrioBackend
-
-    return TrioBackend()
